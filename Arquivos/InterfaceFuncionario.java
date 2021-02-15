bfea654c0d4cd31ba59e7bb3cb513ed32d50d1ac
DerrickPereira1998 <derrickpereira1998@gmail.com>
2021-02-15 07:41:27 -0300

    confirmação de arquivos

--- Arquivos/InterfaceFuncionario.java
+++
@@ -1,8 +0,0 @@
-package projetopoo;
-import java.util.ArrayList;
-public interface InterfaceFuncionario {
-    void cadastrar(Funcionario a) throws Exception;
-    void deletar(Funcionario a) throws Exception;
-    void editar(Funcionario a) throws Exception;
-    ArrayList<Funcionario> listar(Funcionario filtro) throws Exception;
-}
