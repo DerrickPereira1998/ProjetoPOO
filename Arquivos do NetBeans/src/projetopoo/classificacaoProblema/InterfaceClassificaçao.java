8665155f6a7086e27cbb144233f1306f64040951
Derrick André Nascimento Pereira <derrickpereira1998@gmail.com>
2021-02-15 07:09:24 -0300

    Delete Arquivos do NetBeans directory

--- Arquivos do NetBeans/src/projetopoo/classificacaoProblema/InterfaceClassificaçao.java
+++
@@ -1,8 +0,0 @@
-package projetopoo.classificacaoProblema;
-import java.util.ArrayList;
-public interface InterfaceClassificaçao {
-    void cadastrar(ClassificaçaoProblema a) throws Exception;
-    void deletar(ClassificaçaoProblema a) throws Exception;
-    void editar(ClassificaçaoProblema a) throws Exception;
-    ArrayList<ClassificaçaoProblema> listar(ClassificaçaoProblema filtro) throws Exception;
-}
