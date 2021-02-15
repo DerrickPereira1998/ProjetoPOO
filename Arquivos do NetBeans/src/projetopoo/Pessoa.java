1d4fe2f8ad65997cb8ea05386ecc8f1dcb6a7e82
DerrickPereira1998 <derrickpereira1998@gmail.com>
2021-02-15 07:11:04 -0300

    Cancelando arquivos

--- Arquivos do NetBeans/src/projetopoo/Pessoa.java
+++
@@ -1,7 +0,0 @@
-package projetopoo;
-public abstract class Pessoa {
-    String cpf;
-    String rg;
-    String nome;
-}
-
--- Arquivos do NetBeans/src/projetopoo/cliente/InterfaceCliente.java
+++
@@ -1,8 +0,0 @@
-package projetopoo.cliente;
-import java.util.ArrayList;
-public interface InterfaceCliente {
-    void cadastrar(Cliente a) throws Exception;
-    void deletar(Cliente a) throws Exception;
-    void editar(Cliente a) throws Exception;
-    ArrayList<Cliente> listar(Cliente filtro) throws Exception;
-}
